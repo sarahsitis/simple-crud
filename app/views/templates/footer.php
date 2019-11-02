9312a0fb4d1863b70cefe6e14aebbf9c536dc964
sarahsitis <sarahsitis@gmail.com>
2019-11-03 00:55:12 +0700

    menambahkan tag footer dan edit file materialize.css agar footer selalu menempel di bawah (sticky)

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,14 @@
+</main>
+<!-- footer -->
+<footer class="page-footer bottom">
+    <div class="footer-copyright">
+        <div class="container">
+            © 2019 sarahsitis
+            <a class="grey-text text-lighten-4 right" href="#!">materializecss</a>
+        </div>
+    </div>
+</footer>
+
 <!--JavaScript at end of body for optimized loading-->
 <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?= BASEURL; ?>/public/js/materialize.min.js"></script>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,7 +5,7 @@
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="<?= BASEURL; ?>/public/css/materialize.min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="<?= BASEURL; ?>/public/css/materialize.css" media="screen,projection" />
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -50,3 +50,4 @@
 
         <li><a class="waves-effect" href="#!"><i class="material-icons">exit_to_app</i>Logout</a></li>
     </ul>
+    <main>
